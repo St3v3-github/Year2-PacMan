8fef9ef879bfd63eaa4fcf42b56fd6558aa0176e
Steve <steven4.lewis@live.uwe.ac.uk>
2023-05-17 20:55:30 +0100

    CMake Fixed

---
+++ cmake-build-debug/CMakeFiles/PacmanSFML.dir/DependInfo.cmake
@@ -0,0 +1,69 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/tmxsrc/miniz.c" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/tmxsrc/miniz.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "C:/SFML-2.5.1/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/Coin.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/Coin.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/Enemy.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/Enemy.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/Game.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/Game.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/Menus/GameOverMenu.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/Menus/GameOverMenu.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/Menus/MainMenu.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/Menus/MainMenu.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/Player.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/Player.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/ResourceHandler.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/ResourceHandler.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/main.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/main.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/tmxsrc/FreeFuncs.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/tmxsrc/FreeFuncs.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/tmxsrc/ImageLayer.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/tmxsrc/ImageLayer.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/tmxsrc/LayerGroup.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/tmxsrc/LayerGroup.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/tmxsrc/Map.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/tmxsrc/Map.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/tmxsrc/Object.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/tmxsrc/Object.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/tmxsrc/ObjectGroup.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/tmxsrc/ObjectGroup.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/tmxsrc/ObjectTypes.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/tmxsrc/ObjectTypes.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/tmxsrc/Property.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/tmxsrc/Property.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/tmxsrc/TileLayer.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/tmxsrc/TileLayer.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/tmxsrc/Tileset.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/tmxsrc/Tileset.cpp.obj"
+  "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/src/tmxsrc/detail/pugixml.cpp" "C:/Users/Steven/Documents/University/2023 Github Repos/Year2-PacMan/cmake-build-debug/CMakeFiles/PacmanSFML.dir/src/tmxsrc/detail/pugixml.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "C:/SFML-2.5.1/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/PacmanSFML.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PacmanSFML.dir/src/Coin.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/Enemy.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/Game.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/Menus/GameOverMenu.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/Menus/MainMenu.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/Player.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/ResourceHandler.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/main.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/tmxsrc/FreeFuncs.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/tmxsrc/ImageLayer.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/tmxsrc/LayerGroup.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/tmxsrc/Map.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/tmxsrc/Object.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/tmxsrc/ObjectGroup.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/tmxsrc/ObjectTypes.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/tmxsrc/Property.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/tmxsrc/TileLayer.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/tmxsrc/Tileset.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/tmxsrc/detail/pugixml.cpp.obj"
+  "CMakeFiles/PacmanSFML.dir/src/tmxsrc/miniz.c.obj"
+  "PacmanSFML.exe"
+  "PacmanSFML.exe.manifest"
+  "PacmanSFML.pdb"
+  "libPacmanSFML.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/PacmanSFML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
